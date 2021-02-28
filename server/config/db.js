c3d313a7cccfe0f5aea5a968284e034acc4ee63e
Michael Magtibay <mamgtiba@my.centennialcollege.ca>
2021-02-28 08:12:24 -0500

    uploaded mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/book_store"
+    //"URI": "mongodb://localhost/book_store"
+
+     "URI": "mongodb+srv://User:yRQWPsV6HKIQbinK@dbserver.adzdf.mongodb.net/book_store?retryWrites=true&w=majority"
 }
